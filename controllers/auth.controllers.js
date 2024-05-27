dc59c69740bb3747187e206cc7b0aef2556ade34
Sushant Lama <sushantlama732@gmail.com>
2024-05-27 05:57:27 -0700

    added login logout

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,5 +1,7 @@
 import prisma from "../lib/Prisma.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
 
 export const register = async (req, res) => {
   console.log(req.body);
@@ -21,10 +23,32 @@ export const register = async (req, res) => {
   }
 };
 
-export const login = (req, res) => {
-  console.log("Hello from login");
+export const login = async(req, res) => {
+  const {email, password} = req.body
+  console.log(email);
+  console.log(password);
+
+  try {
+    const user = await prisma.user.findUnique({where: {email}})
+    console.log(user);
+    if(!user) return res.status(401).json({message:"Email not found!"})
+     const checkPass = await bcrypt.compare(password, user.password)
+    if(!checkPass) return res.status(401).json({message:"Password is wrong!"})
+
+    const age = 1000* 60 * 24 * 3
+
+  const token = jwt.sign({id: user._id}, process.env.SECRET, {expiresIn: age})
+  res.cookie("token", token, {httpOnly: true, maxAge: age}).status(200).json({message:"Login successfull"})
+    
+  } catch (error) {
+    
+   res.status(400).json(error)
+  }
+ 
+ 
 };
 
+
 export const logout = (req, res) => {
-  console.log("Hello from logout");
+  res.clearCookie("token").status(200).json({message: "Logout successfull"})
 };
